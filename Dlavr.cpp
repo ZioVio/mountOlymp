8e82ab7948c574726d78a0d89d149f3d3cdfdef1
Андрей <anderkoval3@gmail.com>
2019-03-26 16:46:47 +0200

    D not solved =(

---
+++ Dlavr.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+
+int funct(int *arr, int counter, int pidposl);
+
+int main(int argc, char const *argv[])
+{
+    // int n = 0;
+    // cin >> n;
+
+    // float arr[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+    // int m = 0;
+    // cin >> m;
+    // int pidposl = n;
+    // int counter = 0;
+    // // for (int i = 0; i < n; i++) {
+    // //     for (int j = 1; j < pidposl + 1; j++) {
+    // //         if (funct(arr, j) < m)
+    // //             counter++;
+    // //     }
+    // // }
+    // int c = 0;
+    // int subSeqLen = 1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - 1 - i; j++) {
+    //         for (int k = 1; k <= subSeqLen; k++) {
+    //             if ((k+1)/2 > m)
+    //                 c++;
+    //         }
+    //         subSeqLen++;
+    //     }
+    // }
+    // cout << c << endl;
+
+
+
+
+    // НЕ ПОНЯЛИ УСЛОВИЕ
+
+
+    return 0;
+}
+
+// int funct(int *arr, int counter, int pidposl)
+// {
+//     int n = 0;
+//     while()
+//     {
+//     }
+// }
