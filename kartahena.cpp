a73e94d0ee61ae5d8aa7753b3a5add18f6d8fab7
Андрей <anderkoval3@gmail.com>
2019-03-26 15:01:25 +0200

    kartahena

---
+++ kartahena.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int randInt(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
+int indexWithMinElement(int * arr, int n) {
+    int minIndex = 1;
+    int minElem = arr[1];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < minElem) {
+            minIndex = i;
+            minElem = arr[i];
+        }
+    }
+    return minIndex;
+}
+
+int main(int argc, char const *argv[])
+{
+    srand(time(0));
+    int n = 0;
+    cin >> n; 
+    if (n < 1 || n > 100000) {
+        printf("Error: invalid N\n");
+        return 1;
+    }
+    
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; 
+    } 
+
+    int bucket[7];
+    for (int i = 0; i < 7; i++)
+        bucket[i] = 0;
+
+    for (int i = 0; i < n; i++) {
+        bucket[arr[i]]++;
+    }
+      
+
+    int result = bucket[indexWithMinElement(bucket, 7)] + 2;
+    
+    cout << result << endl;
+
+    return 0;
+}
+    
